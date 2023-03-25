1ba0f76607b99c4042d8d9b39b35ee6c0b9c6d09
Mehmet Solak <solakmehmet02@gmail.com>
2023-03-25 23:23:12 +0300

    🐛 Bug: Fix issues on profile and Auth URL

---
+++ auth_test.go
@@ -0,0 +1,49 @@
+package golinkedin
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"log"
+	"strings"
+	"testing"
+)
+
+func Routes() {
+	app := fiber.New()
+
+	app.Get("/", RedirectURL)
+	app.Get("/callback", RetrieveAccessToken)
+	app.Get("/profile", Profile)
+	app.Get("/share", SharePost)
+
+	log.Fatal(app.Listen(":8000"))
+}
+
+func TestInitAuth(t *testing.T) {
+	var credentials = map[string]string{
+		"client_id": "client_id",
+		"redirect_uri": "redirect_uri",
+		"client_secret": "client_secret",
+	}
+
+	InitAuth(credentials)
+	fmt.Println(Config.AuthURL)
+
+	rUrl := Config.AuthURL
+
+	if strings.Contains(rUrl, credentials["client_id"]) && strings.Contains(rUrl, credentials["redirect_uri"]){
+		t.Log("Success, URL is working...")
+	}
+}
+
+func TestRedirectURL(t *testing.T) {
+	var credentials = map[string]string{
+		"client_id": "client_id",
+		"redirect_uri": "redirect_uri",
+		"client_secret": "client_secret",
+	}
+
+	InitAuth(credentials)
+
+	Routes()
+}
